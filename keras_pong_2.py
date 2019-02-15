9afb944d4524e4d76cb09e1f9cfc7785f7a30402
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2019-02-15 12:26:28 +0200

    Some comments

--- keras_pong_2.py
+++ keras_pong_2.py
@@ -2,12 +2,13 @@ import gym
 import numpy as np
 import itertools
 
-from keras.models import Sequential, Model
+from keras.models import Sequential, Model, load_model
 from keras.layers import Dense, Reshape, Dropout, Input
 from keras.optimizers import Adam, Adamax, RMSprop
 import keras.backend as K
 
 import cv2
+import sys
 
 def pong_preprocess_screen(I):
     # Get only the game area as
@@ -20,8 +21,15 @@ def pong_preprocess_screen(I):
     return I.astype(np.float).ravel()
 
 def get_model(input_shape, output_shape, learning_rate=0.001):
+    # You can load a saved model by giving it as a parameter
+    if len(sys.argv) > 1:
+        model = load_model(sys.argv[1])
+        return model, model
     inp = Input(shape=input_shape)
     
+    # The zero-hidden-layer seems to learn quite fast when
+    # presented with hand-fed features. Add layers and CPU time
+    # if you make the input more complex
     model = Sequential([
         #Dense(10, activation="linear", input_shape=input_shape),
         #Dropout(0.2),
@@ -56,15 +64,23 @@ class FeatEnv:
     def __init__(self):
         self.env = gym.make("Pong-v0")
         self.env.env.frameskip = 1
+        self.env.frameskip = 1
         self.reset()
     
     def _get_feat(self):
         #idx = [4, 12, 21, 49, 50, 51, 54, 56, 58, 60, 64, 67, 121, 122]
+        # This reads the paddle and ball positions directly from the
+        # atari emulator's memory. Note that these are available even when
+        # the ball is invisible between rounds (when it goes all the way up).
+        # This hampers the learning somewhat as the paddle tends to go to up
+        # in the beginning.
         idx = [54, 49, 21, 51]
         bally, ballx, oppy, playery = self.env.env._get_ram()[idx].astype(float)/206 - 0.5
         #playery += 38
         #oppy += 38
         #d = self.env.env._get_ram().copy()
+        # Hand feed the difference between horizontal position between ball
+        # and the player's paddle
         return np.array([
             #ballx, bally, oppy - bally,
             playery - bally
@@ -76,8 +92,8 @@ class FeatEnv:
         self.prev_screen = self.current_screen
         #self.current_screen = pong_preprocess_screen(observation)
         self.current_screen = self._get_feat()
-        self.current_feat = np.hstack((self.current_screen, self.prev_screen))
-        #self.current_feat = self.current_screen
+        #self.current_feat = np.hstack((self.current_screen, self.prev_screen))
+        self.current_feat = self.current_screen
         return self.current_feat, reward, done, info
     
     def reset(self):
@@ -85,8 +101,8 @@ class FeatEnv:
         self.env.reset()
         self.current_screen = self._get_feat()
         self.prev_screen = np.zeros(self.current_screen.shape)
-        self.current_feat = np.hstack((self.current_screen, self.prev_screen))
-        #self.current_feat = self.current_screen
+        #self.current_feat = np.hstack((self.current_screen, self.prev_screen))
+        self.current_feat = self.current_screen
         return self.current_feat
 
     def render(self):
@@ -109,16 +125,35 @@ def main(render=True):
         act_ps = []
         feats = []
         while True:
-            if render: env.render()
+            if render:
+                env.render()
+                # If you want to see how the algorithm plays, adding a
+                # sleep here helps
+                #time.sleep(0.02)
             actprobs = model.predict(env.current_feat[np.newaxis], batch_size=1)[0]
             action = np.random.choice(n_actions, p=actprobs)
             
+            # You can try a deterinistic control method like this
+            """
+            d = env.current_feat[0]
+            if abs(d) < 0.03:
+                action = 0
+            elif d > 0:
+                action = 1
+            else:
+                action = 2
+            """
+
             feats.append(env.current_feat)
             feat, reward, done, info = env.step(possible_actions[action])
             rewards.append(reward)
+            # One-hot encode the selected action.
             act_p = np.zeros(n_actions)
             act_p[action] = 1.0
             act_ps.append(act_p)
+            # This would be something like advantage learning and was done
+            # in the original, but I'm not sure this is the correct way of doing it,
+            # so I just use the hot-one.
             #act_ps.append(act_p - actprobs[action])
             if reward != 0 or done:
                 return rewards, act_ps, feats, done
@@ -139,19 +174,29 @@ def main(render=True):
     batch_returns = []
     do_batch = lambda x: np.vstack(list(itertools.chain(*x)))
     while True:
+        # Warning! Hacks here!
         rewards, acts, feats, done = play_round()
         rounds_played += 1
 
         step_rewards = np.zeros(len(rewards))
         running_reward = 0
+
+        # The discounted reward is calculated here, but
+        # it is not actually used
         for step in reversed(range(len(rewards))):
                 running_reward *= discount
                 running_reward += rewards[step]
                 step_rewards[step] = running_reward
+
+        # We actually use non-discounted per-round
+        # mean reward for all of the round's actions
         round_reward = np.mean(rewards)
         step_rewards[:] = round_reward
         batch_rewards.append(rewards)
         #mean_reward += (round_reward - mean_reward)/rounds_played
+        
+        # This is just a running exponential average to
+        # keep track of the progress. Not used in training
         if mean_reward is None:
             mean_reward = round_reward
         else:
@@ -167,10 +212,13 @@ def main(render=True):
         batch_feats.append(feats)
         batch_targets.append(acts)
         batch_returns.append(step_rewards)
-
+        
+        # Every update_freq train using the current
+        # batch
         if rounds_played%update_freq == 0:
             batch_rewards = batch_rewards[-update_freq:]
             print(np.mean(do_batch(batch_rewards)))
+            # Limit the batch to batchlen newest samples
             batch_feats = batch_feats[-batchlen:]
             batch_targets = batch_targets[-batchlen:]
             batch_returns = batch_returns[-batchlen:]
@@ -185,6 +233,10 @@ def main(render=True):
             #a[r < 0,:] = 1 - a[r < 0,:]
             #a /= np.sum(a, axis=1).reshape(-1, 1)
             #trainer.train_on_batch(f, a, sample_weight=r)
+
+            # sample_weight is used here instead of the weird multiplication
+            # of thing in the original. I think this is (at least closer to) the
+            # proper way of doing it in theoretical sense (as in policy gradient).
             trainer.fit(f, a, sample_weight=r)
 
             trainer.save(f"checkpoint.h5")
